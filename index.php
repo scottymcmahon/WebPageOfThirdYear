3ce48ea02cab1313e7c9ceca44a70e44360664dd
scottymcmahon <Swm7018>
2016-10-03 15:53:38 +0100

    Sixth commit of the project with PHP

--- index.php
+++ index.php
@@ -35,5 +35,15 @@
                 }
                 ?>
             </b>
+
+            <b>
+                <?php
+                    $myArray = array("do", "re", "mi"); // declares the array
+                    echo $myArray[0]; // outputs "do"
+                    $myArray[1] = "la"; // modifies position 1 (re)
+                    echo $myArray[1]; // outputs "la"
+                    unset($myArray[2]); // removes the array in position 2
+                ?>
+            </b>
         </body>
     </html>
