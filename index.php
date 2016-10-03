ab644bd7369a34554a65fe4cf437d3d5305c244e
scottymcmahon <Swm7018>
2016-10-03 15:18:52 +0100

    Sixth commit of the project with PHP

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
  * Date: 03/10/2016
  * Time: 15:12
  */
-    <!DOCTYPE html>
-        <html>
-            <head>
-            </head>
-            <body>
-                <p>
-                <?php
-                echo "Hello World";
-                ?>
-                </p>
-            </body>
-        </html>
+<!DOCTYPE html>
+    <html>
+        <head>
+        </head>
+        <body>
+            <p>
+            <?php
+            echo "Hello World";
+            ?>
+            </p>
+        </body>
+    </html>
