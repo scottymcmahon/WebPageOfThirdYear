2e2a79b63034fa1b19607944353305146a11fff7
scottymcmahon <Swm7018>
2016-10-03 16:16:02 +0100

    Sixth commit of the project with PHP

--- index.php
+++ index.php
@@ -1,49 +1,49 @@
 <!DOCTYPE html>
-    <html>
-        <head>
-        </head>
-        <body>
-            <p>
-                <?php
-                    $name = "Scotty";
+<html>
+<head>
+</head>
+<body>
+<p>
+    <?php
+    $name = "Scotty";
 
-                    if($name = "scotty"){
-                        print "Keep that muffin upright!"."<br>";
-                        print "You idiot";
-                    }
-                    else{
-                        print"go away!!";
-                    }
-                ?>
-            </p>
+    if ($name = "scotty") {
+        print "Keep that muffin upright!" . "<br>";
+        print "You idiot";
+    } else {
+        print"go away!!";
+    }
+    ?>
+</p>
 
-            <b>
-                <?php
-                    $numberOfHobbits = 2;
-                    switch ($numberOfHobbits) {
-                        case 1:
-                            echo "1 sad hobbit";
-                            break;
-                        case 2:
-                            echo "2 happy hobbits";
-                            break;
-                        case 3:
-                            echo "3 hobbits are a crowd";
-                            break;
-                        default:
-                            echo "All the hobbits have gone home";
-                }
-                ?>
-            </b>
+<b>
+    <?php
+    $numberOfHobbits = 2;
+    switch ($numberOfHobbits) {
+        case 1:
+            echo "1 sad hobbit";
+            break;
+        case 2:
+            echo "2 happy hobbits";
+            break;
+        case 3:
+            echo "3 hobbits are a crowd";
+            break;
+        default:
+            echo "All the hobbits have gone home";
+    }
+    ?>
+</b>
 
-            <b>
-                <?php
-                    $provisionedActivity = array("specs", "drugs", "rock and roll");//Declaring the array
-                    foreach($provisionedActivity as $x) {
-                        print "<p>$x</p>";
-                    }
-                ?>
+<b>
+    <?php
+    $provisionedActivity = array("specs", "drugs", "rock and roll");//Declaring the array
 
-            </b>
-        </body>
-    </html>
+    foreach ($provisionedActivity as $x) {
+        print "<p>$x</p>";
+    }
+    ?>
+
+</b>
+</body>
+</html>
