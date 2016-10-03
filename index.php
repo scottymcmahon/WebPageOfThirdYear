a182db7b811cc727590b5852616d7f37da2e61b0
scottymcmahon <Swm7018>
2016-10-03 15:26:04 +0100

    Sixth commit of the project with PHP

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
             <?php
             echo "Hello," . " " . "world" . "!";
             echo 5*7;
+            $myname = "Frodo Baggins";
+            $myage = 111;
+            echo "My name is" . $myname . "and I am " . $myage;
             ?>
             </p>
         </body>
