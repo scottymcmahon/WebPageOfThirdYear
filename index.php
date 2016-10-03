d2dd3421c20074425c1a2fd908e50d162d7f4f48
scottymcmahon <Swm7018>
2016-10-03 15:17:28 +0100

    Sixth commit of the project with PHP

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
-<?php
 /**
- * Created by PhpStorm.
+ * Created by Scott McMahon.
  * User: 1404436
  * Date: 03/10/2016
  * Time: 15:12
  */
+    <!DOCTYPE html>
+        <html>
+            <head>
+            </head>
+            <body>
+                <p>
+                <?php
+                echo "Hello World";
+                ?>
+                </p>
+            </body>
+        </html>
