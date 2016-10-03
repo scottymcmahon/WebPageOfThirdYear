7275c0c8e5e0e4bec330954d0dc043f48e5b41bf
scottymcmahon <Swm7018>
2016-10-03 15:24:27 +0100

    Sixth commit of the project with PHP

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
         <body>
             <p>
             <?php
-            echo "Hello World";
             echo "Hello," . " " . "world" . "!";
+            echo 5*7;
             ?>
             </p>
         </body>
