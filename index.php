875ac3b1f0c2c7a65f0cdcd77957bc1a8b40bc02
scottymcmahon <Swm7018>
2016-10-03 15:44:06 +0100

    Sixth commit of the project with PHP

--- index.php
+++ index.php
@@ -3,16 +3,37 @@
         <head>
         </head>
         <body>
-            <?php
-                $name = "Scotty";
+            <p>
+                <?php
+                    $name = "Scotty";
 
-                if($name = "scotty"){
-                    print "Keep that muffin upright! <br>";
-                    print "fdgrgrggbfbgrv";
+                    if($name = "scotty"){
+                        print "Keep that muffin upright! <br>";
+                        print "You idiot";
+                    }
+                    else{
+                        print"go away!!";
+                    }
+                ?>
+            </p>
+
+            <a4>
+                <?php
+                    $numberOfHobbits = 2;
+                    switch ($numberOfHobbits) {
+                        case 1:
+                            echo "1 sad hobbit";
+                            break;
+                        case 2:
+                            echo "2 happy hobbits";
+                            break;
+                        case 3:
+                            echo "3 hobbits are a crowd";
+                            break;
+                        default:
+                            echo "All the hobbits have gone home";
                 }
-                else{
-                    print"go away!!";
-                }
-            ?>
+                ?>
+            </a4>
         </body>
     </html>
